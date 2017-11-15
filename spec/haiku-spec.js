dfc1280046e962f30152f2b51153d1d14545239e
Elliot Burin and Kimberly Bordon <elliot+kimberly@>
2017-11-15 10:59:35 -0800

    add npm, bower, jasmine, karma packages

---
+++ spec/haiku-spec.js
@@ -0,0 +1,15 @@
+var Haiku_maybe_rename = require('./../js/.js').haikuModule;
+
+describe('rename', function(){
+  // var aVar;
+
+  beforeEach(function() {
+    // set up before each test.
+  });
+
+  it('', function() {
+    //where you set up test
+    expect().toEqual();
+  });
+
+});
